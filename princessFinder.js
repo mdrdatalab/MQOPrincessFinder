d19cfe3ebbd4a9a0101d2198e3c6c4cc1999ecda
Michael Dustin Riley Riley <mdrdatalab@gmail.com>
2017-01-31 19:47:51 -0500

    quick fixes
    fixes to a few mistakes in the code now that i've actually tested it

--- princessFinder.js
+++ princessFinder.js
@@ -3,9 +3,9 @@
 function dist(point1, point2){
 	//calculates the distance on a hex grid (the hex tiles can be thought of as 3d cubes, thus the z-axis)
 	//since distances are discrete, an L1-norm is used (maximum difference in coords along)
-	z1 = -(point1[0]+point[1])
-	z2 = -(point2[0]+point[2])
-	return Math.max((abs(point1[0]-point2[0]),abs(point1[1]-point2[1]),abs(z1-z2)))
+	z1 = -(point1[0]+point1[1])
+	z2 = -(point2[0]+point2[1])
+	return Math.max(Math.abs(point1[0]-point2[0]),Math.abs(point1[1]-point2[1]),Math.abs(z1-z2))
 }
 
 
@@ -17,11 +17,11 @@ function possibilities(location, distance, feasibleSet){
 	distmax = distance
 	distmin = distances[distances.indexOf(distance)+1]
 	temp = []
-	if (feasibleSet == []){
+	if (feasibleSet.length == 0){
 		for(var x=0; x<1000; x++){
 			for(var y=0; y<1000; y++){
 				d = dist(location,[x,y])
-				if((d <= distmax) && (dist > distmin){
+				if((d <= distmax) && (d > distmin)){
 					feasibleSet.push([x,y])
 				}
 			}
@@ -30,7 +30,7 @@ function possibilities(location, distance, feasibleSet){
 
 	for (var i=0; i < feasibleSet.length; i++){
 		var x = feasibleSet[i]
-		if((dist(x, loc)) <= distmax) && (dist(x, loc) > distmin){
+		if((dist(x, location) <= distmax) && (dist(x, location) > distmin)){
 			temp.push(x)
 		}
 	}
